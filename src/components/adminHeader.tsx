defa30513911b29cfb47a10ce2d3f7de7f44afcf
savipauk <erik.kranjec8@outlook.com>
2025-04-26 16:11:47 +0200

    Fix admin header

--- src/components/adminHeader.tsx
+++ src/components/adminHeader.tsx
@@ -5,17 +5,16 @@ interface AdminHeaderProps {
   onLogoutToggle: () => void;
 }
 
-
 export default function AdminHeader({ onLogoutToggle }: AdminHeaderProps) {
   return (
     <div className="header">
-      <a className="w-[10%] sm:none md:flex" id="zagreb" href="https://www.zagreb.hr/" target="_blank">
+      <a className="w-[10%] sm:none md:flex items-center justify-center" id="zagreb" href="https://www.zagreb.hr/" target="_blank">
         <img src="/zagreb.svg" alt="Zagreb" />
       </a>
       <div className="w-[80%]">
         <h1>{appTitle}</h1>
       </div>
-      <a className="w-[10%] flex justify-end" href="/">
+      <a className="w-[10%] flex items-center justify-center" href="/">
         Odjavi se
       </a>
     </div>
